4a3c83e0330bd00710413ab837481364563c9283
ringa-nyamvula <nyamvularinga@gmail.com>
2023-07-05 14:12:09 +0300

    Complete assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,20 +3,20 @@ class BirdsController < ApplicationController
   # GET /birds
   def index
     birds = Bird.all
-    render json: birds
+    render json: birds, only: [:name, :species]
   end
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
+    bird = Bird.create(params.permit(:name, :species))
+    render json: bird, only: [:name, :species], status: :created
   end
 
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
-      render json: bird
+      render json: bird, only: [:name, :species]
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
